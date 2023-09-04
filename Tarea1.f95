62f1cab3c7e39af127655257e95b9c1f111db2cb
Israel Garcia <33400384+israelgarciamt@users.noreply.github.com>
2023-09-04 07:47:19 -0600

    Add files via upload

---
+++ Tarea1.f95
@@ -0,0 +1,107 @@
+
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!
+!  Tarea 1 Israel garcia
+!  para la materia de teoria de Automatas
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+program tarea1
+    
+    character(100)::cadena
+    character(19)alfabeto
+    alfabeto=(/i,s,r,a,e,l,g,c,d,o,m,n,u,z,1,6,2,8,./) 
+
+
+
+
+
+write(*,*)"*********************************************"
+write(*,*)"     Tarea 1 Israel Garcia"
+write(*,*)"                1628228"
+write(*,*)"     para la materia de teoria de Automatas"
+write(*,*)"*********************************************"
+
+write(*,*)"Escribe una cadena"
+read(*,*) cadena
+i=1
+
+
+
+if numero(cadena(i))then
+
+
+else
+    if (i==1) then go to 31101
+      
+  
+end if
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if numero(cadena(i))then
+
+else
+
+  
+end if
+
+
+
+
+
+
+
+if (cadena(i)==".")then
+
+else
+
+end if
+
+
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!
+!
+!
+!   Matricula
+!
+!
+!
+!
+
+
+  
+end if
+
+
+
+write(*,*) cadena invalida
+
+
+
+end program
+
+
+
+
+
+
+
+
+
+
